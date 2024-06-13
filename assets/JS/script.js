6d8c4b213e786038bcbd4683d65a3e91c15b65b3
Fleepx <felipesanchez.aq@gmail.com>
2024-06-13 14:12:37 -0400

    Terminado

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,22 @@
-/*var nombreUsuario = prompt("Por favor, ingresa tu nombre.");
+document.addEventListener("DOMContentLoaded", function() {
+var nombreUsuario = prompt("Por favor, ingresa tu nombre.");
 var saludo = document.querySelector("#nombreUsuario")
+var mensajeBienvenida = document.querySelector("#bienvenida");
 
 if(nombreUsuario != null && nombreUsuario !== "") {
     saludo.textContent = " "+nombreUsuario;
+    mensajeBienvenida.style.display = "flex";
 } else {
     saludo.textContent = "";
-}*/
+    mensajeBienvenida.style.display = "flex";
+}});
 
+document.addEventListener("DOMContentLoaded", function() {
+    var mensajeBienvenida = document.querySelector("#bienvenida");
+    var contenidoAdicional = document.querySelector("#desafio1");
 
+    mensajeBienvenida.addEventListener("animationend", function() {
+        mensajeBienvenida.style.display = "none";
+        contenidoAdicional.style.display = "flex";
+    });
+});
