abdf77a84a183c64cdeb252bda4dda30de987aef
Fleepx <felipesanchez.aq@gmail.com>
2024-06-12 20:18:31 -0400

    First

---
+++ assets/JS/script.js
@@ -0,0 +1,8 @@
+/*var nombreUsuario = prompt("Por favor, ingresa tu nombre.");
+var saludo = document.querySelector("#nombreUsuario")
+
+if(nombreUsuario != null && nombreUsuario !== "") {
+    saludo.textContent = " "+nombreUsuario;
+} else {
+    saludo.textContent = "";
+}*/
